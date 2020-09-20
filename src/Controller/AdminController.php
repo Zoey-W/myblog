7a7636517e3837037ce6bbfc032515e34f582dad
ZoeyW <zowy0628@163.com>
2020-09-20 17:24:14 +0800

    second iteration

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
+use App\Form\PostFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,9 +32,9 @@ class AdminController extends AbstractController
     {
         if ($this->authorRepository->findOneByUsername($this->getUser()->getUserName())) {
             
-            $this->addFlash('error', 'Unable to create author, author already exists!');
-
-            return $this->redirectToRoute('homepage');
+            //$this->addFlash('error', 'Unable to create author, author already exists!');
+            $request->getSession()->set('user_is_author', true);
+            return $this->redirectToRoute('admin_index');
         }
 
         $author = new Author();
@@ -43,7 +45,7 @@ class AdminController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->entityManager->persist($author);
-            $this->entityManager->flush($author);
+            $this->entityManager->flush();
 
             $request->getSession()->set('user_is_author', true);
             $this->addFlash('success', 'Congratulations! You are now an author.');
@@ -53,6 +55,105 @@ class AdminController extends AbstractController
 
         return $this->render('admin/create_author.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin", name="admin_index")
+     */
+    public function indexAction()
+    {
+        $author = $this->authorRepository->findOneByUsername($this->getUser()->getUsername());
+        $blogPosts = [];
+
+
+        $blogPosts = $this->blogPostRepository->findByAuthor($author);
+
+
+        return $this->render('admin/index.html.twig',['blogPosts' => $blogPosts]);
+    }
+
+    /**
+     * @Route ("/admin/create-post", name="admin_create_post")
+     */
+    public function createPostAction(Request $request)
+    {
+        $blogPost = new BlogPost();
+        $author = $this->authorRepository->findOneByUsername($this->getUser()->getUsername());
+        $blogPost->setAuthor($author);
+
+        $form = $this->createForm(PostFormType::class, $blogPost);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->entityManager->persist($blogPost);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Congratulations! Your post is created');
+            return $this->redirectToRoute('admin_index');
+        }
+
+        return $this->render('admin/create_post.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route ("/admin/delete-post/{postId}", name="admin_delete_post")
+     */
+    public function deletePostAction($postId)
+    {
+        $blogPost = $this->blogPostRepository->findOneById($postId);
+        $author = $this->authorRepository->findOneByUsername($this->getUser()->getUsername());
+
+        if(!$blogPost || $author !== $blogPost->getAuthor()){
+            $this->addFlash('error', 'Unable to delete post!');
+            return $this->redirectToRoute('admin_index');
+        }
+
+        $this->entityManager->remove($blogPost);
+        $this->entityManager->flush();
+
+        $this->addFlash('success', 'Post was deleted!');
+
+        return $this->redirectToRoute('admin_index');
+    }
+
+    /**
+     * @Route ("/admin/edit-post/{slug}", name="admin_edit_post")
+     */
+    public function editPostAction($slug, Request $request)
+    {
+        $blogPost = $this->blogPostRepository->findOneBySlug($slug);
+        $form = $this->createForm(PostFormType::class, $blogPost);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Changes have been saved!');
+            return $this->redirectToRoute('admin_index');
+        }
+        return $this->render('admin/edit_post.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route ("/admin/show/{slug}", name="admin_show_post")
+     */
+    //这是用户查看自己写的博客，会有删除、回复评论等功能
+    public function showPostAction($slug)
+    {
+        $blogPost = $this->blogPostRepository->findOneBySlug($slug);
+
+        if(!$blogPost){
+            $this->addFlash('error', 'Unable to find post!');
+            return $this->redirectToRoute('admin_index');
+        }
+        return $this->render('admin/show_post.html.twig',[
+            'blogPost' => $blogPost
         ]);
     }
 }
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,65 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BlogController extends AbstractController
 {
+    private $entityManager;
+    private $authorRepository;
+    private $blogPostRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('App:Author');
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'blogPosts' => $this->blogPostRepository->findAll()
         ]);
     }
+
+    /**
+     * @Route ("/show/{slug}", name="show_post")
+     */
+    public function showPostAction($slug){
+        //是否根据session来判断要展示的内容
+        $blogPost = $this->blogPostRepository->findOneBySlug($slug);
+
+        if(!$blogPost){
+            $this->addFlash('error', 'Unable to find post!');
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('blog/show_post.html.twig',[
+            'blogPost' => $blogPost
+        ]);
+    }
+
+    /**
+     * @Route ("/show/author/{name}", name="show_author")
+     */
+    public function showAuthorAction($name){
+        $author = $this->authorRepository->findOneByName($name);
+        $blogPosts = [];
+        $blogPosts = $this->blogPostRepository->findByAuthor($author);
+
+        if(!$author){
+            $this->addFlash('error', 'Unable to find author!');
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('blog/show_author.html.twig',[
+            'author' => $author,
+            'blogPosts' => $blogPosts
+        ]);
+    }
+
 }
